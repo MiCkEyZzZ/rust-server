4616c1f618bb0545d4d171582b2af5bc7a98be8f
Cry <m.f.zuravlev@yandex.ru>
2023-01-08 17:23:42 +0500

    added receiving input

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 extern crate actix_web;
 
-use actix_web::{get, middleware, web, App, HttpServer, Result};
-use serde::Serialize;
+use actix_web::{get, middleware, post, web, App, HttpServer, Result};
+use serde::{Deserialize, Serialize};
 use std::cell::Cell;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
@@ -22,6 +22,18 @@ struct IndexResponse {
     messages: Vec<String>,
 }
 
+#[derive(Deserialize)]
+struct PostInput {
+    message: String,
+}
+
+#[derive(Serialize)]
+struct PostResponse {
+    server_id: usize,
+    request_count: usize,
+    message: String,
+}
+
 #[get("/")]
 async fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
     let request_count = state.request_count.get() + 1;
@@ -32,6 +44,36 @@ async fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
         server_id: state.server_id,
         request_count,
         messages: ms.clone(),
+    }))
+}
+
+async fn post(
+    msg: web::Json<PostInput>,
+    state: web::Data<AppState>,
+) -> Result<web::Json<PostResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let mut ms = state.messages.lock().unwrap();
+    ms.push(msg.message.clone());
+
+    Ok(web::Json(PostResponse {
+        server_id: state.server_id,
+        request_count,
+        message: msg.message.clone(),
+    }))
+}
+
+#[post("/clear")]
+async fn clear(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let mut ms = state.messages.lock().unwrap();
+    ms.clear();
+
+    Ok(web::Json(IndexResponse {
+        server_id: state.server_id,
+        request_count,
+        messages: vec![],
     }))
 }
 
@@ -58,6 +100,12 @@ impl RustServerApp {
                 }))
                 .wrap(middleware::Logger::default())
                 .service(index)
+                .service(
+                    web::resource("/send")
+                        .app_data(web::JsonConfig::default().limit(4096))
+                        .route(web::post().to(post)),
+                )
+                .service(clear)
         })
         .bind(("127.0.0.1", self.port))?
         .workers(8)
